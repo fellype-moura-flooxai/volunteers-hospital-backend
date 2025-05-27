5d4cc7cb59b3868f2b3e299150097b5f8da3e89f
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-27 16:59:50 -0300

    feat: connection with railway database + dotenv configured

--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'voluntarios_hospital'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect(err => {
